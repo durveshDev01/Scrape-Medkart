c0717a33258b43aa9559b225f86ea66d2bab2833
doShant-dev <gdoshant@gmail.com>
2025-01-26 19:36:07 +0530

    Implemented Login

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
+
+
 mongoose.connect('mongodb+srv://durveshDev01:dbDurvesh0001@college-project.dijxg.mongodb.net/?retryWrites=true&w=majority&appName=College-Project');
 
 const UserSchema = mongoose.Schema({email: String, name: String, password: String, address: [String]});
 const User = mongoose.model('User', UserSchema);
+
+
 
 export {User}
